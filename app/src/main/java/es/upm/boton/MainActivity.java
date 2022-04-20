c92ac09afbc7b54b5018d904a60056e50ac84d67
btabuenca <bernardo.tabuenca@gmail.com>
2022-04-20 15:00:40 +0200

    Rebase version 2022

---
+++ app/src/main/java/es/upm/boton/MainActivity.java
@@ -0,0 +1,38 @@
+package es.upm.boton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button buttonNext, buttonPrevious;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonNext = findViewById(R.id.button_next);
+        buttonPrevious = findViewById(R.id.button_previous);
+
+        buttonNext.setOnClickListener(this);
+        buttonPrevious.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.button_next) {
+            Toast.makeText(this, "Siguiente", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.button_previous) {
+            Toast.makeText(this, "Anterior", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
